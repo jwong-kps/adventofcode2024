a935ffa940174a1ced5f8ad83559ba62784f187b
Jonathan Wong <jonathan.wong@kps.com>
2024-12-08 22:09:07 +0000

    Moved the project build system to amper

--- app/input/README.md
+++
@@ -1,7 +0,0 @@
-Inputs to puzzles are located here.
-
-i.e.
-
-input/day01.txt
-
-input/examples/day01.txt
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,14 @@
-// The settings file is the entry point of every Gradle build.
-// Its primary purpose is to define the subprojects.
-// It is also used for some aspects of project-wide configuration, like managing plugins, dependencies, etc.
-// https://docs.gradle.org/current/userguide/settings_file_basics.html
-
-dependencyResolutionManagement {
-    // Use Maven Central as the default repository (where Gradle will download dependencies) in all subprojects.
-    @Suppress("UnstableApiUsage")
+pluginManagement {
     repositories {
         mavenCentral()
+        google()
+        gradlePluginPortal()
+        maven("https://packages.jetbrains.team/maven/p/amper/amper")
+        maven("https://www.jetbrains.com/intellij-repository/releases")
+        maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
     }
 }
 
 plugins {
-    // Use the Foojay Toolchains plugin to automatically download JDKs required by subprojects.
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.jetbrains.amper.settings.plugin").version("0.5.0")
 }
-
-include(":app")
-
-rootProject.name = "adventofcode2024"
