531c6b2006dccf48f817f94bb3717baa6ca1d3c5
Jonathan Wong <jonathan.wong@kps.com>
2024-12-10 12:03:56 +0000

    Code formatting

--- src/Day09.kt
+++ src/Day09.kt
@@ -11,6 +11,7 @@ class Day09 : Challenge("day09", false) {
 
         for (i in diskMapAsBlocks.indices.reversed()) {
             val freeBlockIndex = diskMapAsBlocks.indexOf(freeSpaceBlockIndicator)
+
             if (freeBlockIndex < i) {
                 diskMapAsBlocks[freeBlockIndex] = diskMapAsBlocks[i]
                 diskMapAsBlocks[i] = freeSpaceBlockIndicator
@@ -20,39 +21,17 @@ class Day09 : Challenge("day09", false) {
         return calculateCheckSum(diskMapAsBlocks).toString()
     }
 
-    private fun generateBlockDiskMap(): MutableList<Long> {
-        val diskMapAsBlocks = mutableListOf<Long>()
-        var processFreeSpace = false
-        var blockId: Long = 0
-
-        for (char in diskMap[0]) {
-            val blockSize = char.toString().toLong()
-            for (i in 1..blockSize) {
-                diskMapAsBlocks.add(if (processFreeSpace) freeSpaceBlockIndicator else blockId)
-            }
-            if (!processFreeSpace) {
-                blockIdToBlockSize[blockId] = blockSize
-                blockId++
-            }
-            processFreeSpace = !processFreeSpace
-        }
-
-        return diskMapAsBlocks
-    }
-
-    private fun calculateCheckSum(diskMapAsBlocks: List<Long>): Long = diskMapAsBlocks.filter { it >= 0 }.foldIndexed(0) { index, acc, value -> acc + value * index }
-
     override fun part2(): String {
-        blockIdToBlockSize = mutableMapOf()
         val diskMapAsBlocks = generateBlockDiskMap()
 
         for (key in blockIdToBlockSize.keys.reversed()) {
             val index = findIndexOfFreeSpaceForBlockSize(blockIdToBlockSize.getOrDefault(key, 0), diskMapAsBlocks)
-            val indexOfBlock = diskMapAsBlocks.indexOf(key)
-            if (index == -1 || index > indexOfBlock) continue
-            diskMapAsBlocks.replaceAll { if (it == key) freeSpaceBlockIndicator else it }
-            for (i in index..(index - 1 + blockIdToBlockSize[key]!!)) {
-                diskMapAsBlocks[i.toInt()] = key
+
+            if (index >= 0 && index < diskMapAsBlocks.indexOf(key)) {
+                diskMapAsBlocks.replaceAll { if (it == key) freeSpaceBlockIndicator else it }
+
+                for (i in index..(index - 1 + blockIdToBlockSize[key]!!))
+                    diskMapAsBlocks[i.toInt()] = key
             }
         }
 
@@ -61,25 +40,51 @@ class Day09 : Challenge("day09", false) {
         return calculateCheckSum(diskMapAsBlocks).toString()
     }
 
+    private fun generateBlockDiskMap(): MutableList<Long> {
+        val diskMapAsBlocks = mutableListOf<Long>()
+        blockIdToBlockSize = mutableMapOf()
+        var processFreeSpace = false
+        var blockId: Long = 0
+
+        for (char in diskMap[0]) {
+            val blockSize = char.toString().toLong()
+
+            for (i in 1..blockSize) {
+                diskMapAsBlocks.add(if (processFreeSpace) freeSpaceBlockIndicator else blockId)
+            }
+
+            if (!processFreeSpace) {
+                blockIdToBlockSize[blockId] = blockSize
+                blockId++
+            }
+
+            processFreeSpace = !processFreeSpace
+        }
+
+        return diskMapAsBlocks
+    }
+
     private fun findIndexOfFreeSpaceForBlockSize(blockSize: Long, diskMapAsBlocks: MutableList<Long>): Int {
         var spaceFound: Boolean
-        var index = -1
+
         for (i in diskMapAsBlocks.indices) {
             if (diskMapAsBlocks[i] == freeSpaceBlockIndicator) {
                 spaceFound = true
-                for (j in (i + 1) until i + blockSize) {
-                    if (j.toInt() == diskMapAsBlocks.size || diskMapAsBlocks[j.toInt()] != freeSpaceBlockIndicator) {
+
+                for (j in (i + 1) until (i + blockSize)) {
+                    if (j.toInt() >= diskMapAsBlocks.size || diskMapAsBlocks[j.toInt()] != freeSpaceBlockIndicator) {
                         spaceFound = false
                         break
                     }
                 }
-                if (spaceFound) {
-                    index = i
-                    break
-                }
+
+                if (spaceFound)
+                    return i
             }
         }
 
-        return index
+        return -1
     }
+
+    private fun calculateCheckSum(diskMapAsBlocks: List<Long>): Long = diskMapAsBlocks.filter { it >= 0 }.foldIndexed(0) { index, acc, value -> acc + value * index }
 }
