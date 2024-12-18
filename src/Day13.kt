7f5c2084ac8320d90b708c6bc1c513550aa61488
Jonathan Wong <jonathan.wong@kps.com>
2024-12-18 22:47:38 +0000

    Completed Day 13 Challenge 02

--- src/Day13.kt
+++ src/Day13.kt
@@ -6,33 +6,33 @@ class Day13 : Challenge("day13", false) {
     private val regex = "\\d+".toRegex()
 
     override fun part1(): String {
-        var ax = 0
-        var ay = 0
-        var bx = 0
-        var by = 0
-        var px: Int
-        var py: Int
+        var ax: Double = 0.0
+        var ay: Double = 0.0
+        var bx: Double = 0.0
+        var by: Double = 0.0
+        var px: Double = 0.0
+        var py: Double = 0.0
 
-        var count = 0
+        var count: Long = 0L
 
         for (line in lines) {
             val numbers = regex.findAll(line).map { it.value }.toList()
             if (line.startsWith("Button A")) {
-                ax = numbers[0].toInt()
-                ay = numbers[1].toInt()
+                ax = numbers[0].toDouble()
+                ay = numbers[1].toDouble()
             } else if (line.startsWith("Button B")) {
-                bx = numbers[0].toInt()
-                by = numbers[1].toInt()
+                bx = numbers[0].toDouble()
+                by = numbers[1].toDouble()
             } else if (line.startsWith("Prize")) {
-                px = numbers[0].toInt()
-                py = numbers[1].toInt()
+                px = numbers[0].toDouble()
+                py = numbers[1].toDouble()
 
-                if ((by * ax - bx * ay) == 0 || (ay * bx - ax * by) == 0) continue
+                if ((by * ax - bx * ay) == 0.0 || (ay * bx - ax * by) == 0.0) continue
 
                 val buttonA = solveForButtonA(ax, ay, bx, by, px, py)
                 val buttonB = solveForButtonB(ax, ay, bx, by, px, py)
 
-                if (!hasNonZeroDecimalPart(buttonA.toString()) && !hasNonZeroDecimalPart(buttonB.toString())) count += buttonA.toInt() * 3 + buttonB.toInt()
+                if (buttonA % 1.0 == 0.0 && buttonB % 1.0 == 0.0) count += buttonA.toLong() * 3 + buttonB.toLong()
             }
         }
 
@@ -41,7 +41,7 @@ class Day13 : Challenge("day13", false) {
 
     private fun hasNonZeroDecimalPart(numberString: String): Boolean {
         val parts = numberString.split(".")
-        return parts.size == 2 && parts[1].toIntOrNull() != 0
+        return parts.size == 2 && parts[1].toDoubleOrNull() != 0.0
     }
 
     /*
@@ -62,13 +62,43 @@ class Day13 : Challenge("day13", false) {
       ay * px - ax * py = bCount(ay * bx - ax * by)
       bCount = (ay * px - ax * py) / (ay * bx - ax * by)
      */
-    private fun solveForButtonA(ax: Int, ay: Int, bx: Int, by: Int, px: Int, py: Int): Double =
-        (by.toDouble() * px.toDouble() - bx.toDouble() * py.toDouble()) / (by.toDouble() * ax.toDouble() - bx.toDouble() * ay.toDouble())
+    private fun solveForButtonA(ax: Double, ay: Double, bx: Double, by: Double, px: Double, py: Double): Double =
+        (by * px - bx * py) / (by * ax - bx * ay)
 
-    private fun solveForButtonB(ax: Int, ay: Int, bx: Int, by: Int, px: Int, py: Int): Double =
-        (ay.toDouble() * px.toDouble() - ax.toDouble() * py.toDouble()) / (ay.toDouble() * bx.toDouble() - ax.toDouble() * by.toDouble())
+    private fun solveForButtonB(ax: Double, ay: Double, bx: Double, by: Double, px: Double, py: Double): Double =
+        (ay * px - ax * py) / (ay * bx - ax * by)
 
     override fun part2(): String {
-        return ""
+        var ax: Double = 0.0
+        var ay: Double = 0.0
+        var bx: Double = 0.0
+        var by: Double = 0.0
+        var px: Double = 0.0
+        var py: Double = 0.0
+
+        var count: Long = 0L
+
+        for (line in lines) {
+            val numbers = regex.findAll(line).map { it.value }.toList()
+            if (line.startsWith("Button A")) {
+                ax = numbers[0].toDouble()
+                ay = numbers[1].toDouble()
+            } else if (line.startsWith("Button B")) {
+                bx = numbers[0].toDouble()
+                by = numbers[1].toDouble()
+            } else if (line.startsWith("Prize")) {
+                px = numbers[0].toDouble() + 10000000000000.0
+                py = numbers[1].toDouble() + 10000000000000.0
+
+                if ((by * ax - bx * ay) == 0.0 || (ay * bx - ax * by) == 0.0) continue
+
+                val buttonA = solveForButtonA(ax, ay, bx, by, px, py)
+                val buttonB = solveForButtonB(ax, ay, bx, by, px, py)
+
+                if (buttonA % 1.0 == 0.0 && buttonB % 1.0 == 0.0) count += buttonA.toLong() * 3 + buttonB.toLong()
+            }
+        }
+
+        return count.toString()
     }
 }
